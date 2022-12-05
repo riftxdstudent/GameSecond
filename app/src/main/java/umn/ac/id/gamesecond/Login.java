d4a80883840d29f16ea51dd952da0ad2f935d7d7
riftxdstudent <muhammad.rifqi2@student.umn.ac.id>
2022-12-05 19:26:09 +0700

    make login page

---
+++ app/src/main/java/umn/ac/id/gamesecond/Login.java
@@ -0,0 +1,80 @@
+package umn.ac.id.gamesecond;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    EditText etEmail, etPassword;
+    MaterialButton btnLogin;
+    TextView loginToRegister;
+    ProgressBar pbLogin;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getActionBar().hide();
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (MaterialButton) findViewById(R.id.btnLogin);
+        loginToRegister = (TextView) findViewById(R.id.loginToRegister);
+        pbLogin = (ProgressBar) findViewById(R.id.pbLogin);
+        fAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    etEmail.setError("Email is required!");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    etPassword.setError("Password is Required");
+                    return;
+                }
+
+                if(password.length() < 5) {
+                    etPassword.setError("Password must be more than 5 characters");
+                    return;
+                }
+
+                pbLogin.setVisibility(View.VISIBLE);
+
+                //Cek user di Firebase
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Login.this, "Cannot Login, Email or Password incorrect!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
